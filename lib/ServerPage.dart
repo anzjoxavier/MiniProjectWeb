a8e4669b8d3f2a89730664983e32dbb4d94b3dd2
ANZ JO XAVIER <116029351+anzjoxavier@users.noreply.github.com>
2023-06-14 00:37:58 +0530

    Done Speech and Voice

--- lib/ServerPage.dart
+++ lib/ServerPage.dart
@@ -1,17 +1,27 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:projectapp/constants.dart';
 
+// ignore: must_be_immutable
 class ServerPage extends StatelessWidget {
+  String voiceText = 'Content is Loading. Please Wait';
   final String sentance;
   final String dropdownvalue;
-  const ServerPage({
+  ServerPage({
     Key? key,
     required this.sentance,
     required this.dropdownvalue,
   }) : super(key: key);
+
+  final FlutterTts flutterTts = FlutterTts();
+  speak() async {
+    await flutterTts.setLanguage('en-US');
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(voiceText);
+  }
 
   Future<String> getSarcastic() async {
     final response = await http.post(
@@ -45,32 +55,54 @@ class ServerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          
-          colors: [
-              Colors.redAccent, 
-              Colors.blueAccent,
-               Colors.redAccent,
-              //add more colors
-            ]
+        floatingActionButton:         CircleAvatar(
+          backgroundColor: Colors.green,
+          radius: 20,
+          child: IconButton(
+            
+            padding: EdgeInsets.zero,
+            icon:const Icon(Icons.volume_up),
+            color: Colors.white,
+            onPressed: () {
+              speak();
+            },
           ),
-      ),
-    ),
-        // backgroundColor:Color.fromARGB(255, 251, 0, 125).withOpacity(0.8),
-        title:const Text(
-          "Result",
-          style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w700),
         ),
-      ),
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back)),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.redAccent,
+                Colors.blueAccent,
+                Colors.redAccent,
+                //add more colors
+              ]),
+            ),
+          ),
+          // backgroundColor:Color.fromARGB(255, 251, 0, 125).withOpacity(0.8),
+          title: const Text(
+            "Result",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),
+          ),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Given Comment: $sentance",style:const TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-              const SizedBox(height: 25,),
+              Text(
+                "Given Comment: $sentance",
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
               FutureBuilder(
                 builder: (ctx, snapshot) {
                   // Checking if future is resolved or not
@@ -83,14 +115,21 @@ class ServerPage extends StatelessWidget {
                           style: const TextStyle(fontSize: 18),
                         ),
                       );
-          
+
                       // if we got our data
                     } else if (snapshot.hasData) {
                       // Extracting data from snapshot object
+
                       const JsonDecoder decoder = JsonDecoder();
                       final Map<String, dynamic> object =
                           decoder.convert(snapshot.data!);
                       if (dropdownvalue == 'Sarcasm Detection') {
+                        object['is_sarcastic'] == 1
+                            ? voiceText =
+                                "Given Comment, $sentance is sarcastic"
+                            : voiceText =
+                                "Given Comment, $sentance is not sarcastic";
+                        speak();
                         return Center(
                             child: SizedBox(
                           height: 0.60 * MediaQuery.of(context).size.height,
@@ -100,12 +139,19 @@ class ServerPage extends StatelessWidget {
                               : Image.network("assets/Not Sarcasm.png"),
                         ));
                       } else {
+                        object['toxic']['toxic'] == 1
+                            ? voiceText =
+                                "Given Comment, $sentance is toxic"
+                            : voiceText =
+                                "Given Comment, $sentance is not toxic";
+                        speak();
                         return Center(
-                         
-                          child: SizedBox(
-                                      height:0.60 * MediaQuery.of(context).size.height,
-                                      width: 0.60 * MediaQuery.of(context).size.width,
-                                      child: object['toxic']['toxic'] ==1 ? Image.network("assets/toxic.png"): Image.network("assets/nontoxic.png"),
+                            child: SizedBox(
+                          height: 0.60 * MediaQuery.of(context).size.height,
+                          width: 0.60 * MediaQuery.of(context).size.width,
+                          child: object['toxic']['toxic'] == 1
+                              ? Image.network("assets/toxic.png")
+                              : Image.network("assets/nontoxic.png"),
                         ));
                       }
                     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
-
+import 'dart:html';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter/material.dart';
 
 import 'package:projectapp/ServerPage.dart';
@@ -15,10 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-      ),
+      title: 'Sarcasm and Toxicity Analyser',
+      theme: ThemeData(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -34,6 +33,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _textEditingController = TextEditingController();
+
+  stt.SpeechToText speech = stt.SpeechToText();
+  Color iconColor = Colors.white;
+  // bool _isSpeechStarted = false;
+  bool _isListening = false;
+
+  // bool _isEndOfSpeech = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    speech = stt.SpeechToText();
+    super.initState();
+  }
+
+  void onListen() async {
+    if (!_isListening) {
+      bool available = await speech.initialize(
+        onStatus: (val) => print('onStatus: $val'),
+        onError: (val) => print('onError: $val'),
+      );
+      if (available) {
+        setState(() {
+          _isListening = true;
+          iconColor = Colors.red;
+        });
+        speech.listen(
+          listenMode: stt.ListenMode.search,
+          onResult: (val) => setState(() {
+            print(val.recognizedWords);
+            _textEditingController.text = val.recognizedWords;
+          }),
+        );
+      } else {
+        setState(() {
+          _isListening = false;
+          iconColor = Colors.white;
+          speech.stop();
+        });
+      }
+    }
+  }
+
+  void stopListening() {
+    setState(() {
+      _isListening = false;
+      iconColor = Colors.white;
+      speech.stop();
+    });
+  }
+
   String dropdownvalue = 'Sarcasm Detection';
   var items = ["Sarcasm Detection", "Toxicity Detection"];
   @override
@@ -41,39 +91,36 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         flexibleSpace: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          
-          colors: [
-              Colors.redAccent, 
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.redAccent,
               Colors.blueAccent,
-               Colors.redAccent,
+              Colors.redAccent,
               //add more colors
-            ]
+            ]),
           ),
-      ),
-    ),
+        ),
         // backgroundColor:Color.fromARGB(255, 251, 0, 125).withOpacity(0.8),
         title: const Center(
             child: Text(
           "Sarcasm and Toxicity Detector",
-          style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w700),
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),
         )),
       ),
-      body: Stack(
-        children:[ 
-          SizedBox(
-            
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: Image.network("assets/background.png",fit: BoxFit.fill,),
+      body: Stack(children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Image.network(
+            "assets/background.png",
+            fit: BoxFit.fill,
           ),
-          
-          SizedBox(
+        ),
+        SizedBox(
           width: MediaQuery.of(context).size.width,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-          
             children: [
               const SizedBox(
                 height: 50,
@@ -81,34 +128,43 @@ class _MyHomePageState extends State<MyHomePage> {
               SizedBox(
                 width: 250,
                 child: DecoratedBox(
-                      decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            colors: [
-              Colors.redAccent, 
-              Colors.blueAccent,
-              Colors.purpleAccent
-              //add more colors
-            ]),
-          borderRadius: BorderRadius.circular(25),
-          boxShadow: const <BoxShadow>[
-            BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.57), //shadow for button
-                blurRadius: 5) //blur radius of shadow
-          ]
-    ),
+                  decoration: BoxDecoration(
+                      gradient: const LinearGradient(colors: [
+                        Colors.redAccent,
+                        Colors.blueAccent,
+                        Colors.purpleAccent
+                        //add more colors
+                      ]),
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: const <BoxShadow>[
+                        BoxShadow(
+                            color: Color.fromRGBO(
+                                0, 0, 0, 0.57), //shadow for button
+                            blurRadius: 5) //blur radius of shadow
+                      ]),
                   child: Padding(
-                    padding:const EdgeInsets.only(left:15, right:15),
+                    padding: const EdgeInsets.only(left: 15, right: 15),
                     child: DropdownButton(
                       borderRadius: BorderRadius.circular(20),
-                      dropdownColor: const Color.fromARGB(255, 251, 0, 125).withOpacity(0.75),
+                      dropdownColor: const Color.fromARGB(255, 251, 0, 125)
+                          .withOpacity(0.75),
                       isExpanded: true,
                       value: dropdownvalue,
-                      underline: Container(), 
-                      icon: const Icon(Icons.keyboard_arrow_down,color: Colors.white,),
+                      underline: Container(),
+                      icon: const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.white,
+                      ),
                       items: items.map((String items) {
                         return DropdownMenuItem(
                           value: items,
-                          child: Center(child: Text(items,style:const TextStyle(color: Colors.white,fontWeight: FontWeight.w700),)),
+                          child: Center(
+                              child: Text(
+                            items,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          )),
                         );
                       }).toList(),
                       onChanged: (String? newValue) {
@@ -124,70 +180,81 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 35,
               ),
               SizedBox(
-                width: 0.60*MediaQuery.of(context).size.width,
+                width: 0.60 * MediaQuery.of(context).size.width,
                 child: TextField(
-                  
                   controller: _textEditingController,
                   decoration: InputDecoration(
-                    prefixIcon:const Icon(Icons.search),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.mic,
+                        color: iconColor,
+                      ),
+                      onPressed: () {
+                        if (_isListening == false) {
+                          onListen();
+                        } else {
+                          stopListening();
+                        }
+                      },
+                    ),
+                    suffixIconColor: Colors.amber,
+                    prefixIcon: const Icon(Icons.search),
                     hintText: "Type your Comment",
-                    hintStyle: TextStyle(color:Colors.grey[350]),
+                    hintStyle: TextStyle(color: Colors.grey[350]),
                     fillColor: Colors.grey[50]!.withOpacity(0.50),
                     filled: true,
                     border: OutlineInputBorder(
-                        
-                        borderSide: const BorderSide(color:Colors.amber),
+                        borderSide: const BorderSide(color: Colors.amber),
                         borderRadius: BorderRadius.circular(50)),
                   ),
-                
                 ),
               ),
               const SizedBox(
                 height: 30,
               ),
               ElevatedButton(
-                
-                onPressed: (){
-                  if(_textEditingController.text.isNotEmpty){
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (ctx) => ServerPage(
-                        sentance: _textEditingController.text,
-                        dropdownvalue:dropdownvalue
-                      ),
-                    ));
-                  }else{
-               final snackBar = SnackBar(
-               content: const Text('Text is Empty'),
-               backgroundColor: (Colors.black12),
-               action: SnackBarAction(
-                label: 'dismiss',
                 onPressed: () {
-                },
-              ),
-            );
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  if (_textEditingController.text.isNotEmpty) {
+                    if (_isListening == true) {
+                      stopListening();
+                    }
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (ctx) => ServerPage(
+                              sentance: _textEditingController.text,
+                              dropdownvalue: dropdownvalue),
+                        ));
+                  } else {
+                    final snackBar = SnackBar(
+                      content: const Text('Text is Empty'),
+                      backgroundColor: (Colors.black12),
+                      action: SnackBarAction(
+                        label: 'dismiss',
+                        onPressed: () {},
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   }
-                  
-                } ,
+                },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
-
                 ),
-              
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: SizedBox(
-                    width: 0.05*MediaQuery.of(context).size.width,
+                    width: 0.05 * MediaQuery.of(context).size.width,
                     child: const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         ImageIcon(NetworkImage("assets/tree.png")),
-                        SizedBox(width: 8,),
+                        SizedBox(
+                          width: 8,
+                        ),
                         Text(
                           'Find',
-                          style: TextStyle(color: Colors.white,fontWeight: FontWeight.w600),
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w600),
                         ),
                       ],
                     ),
@@ -196,9 +263,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-        ),]
-      ),
+        ),
+      ]),
     );
   }
 }
-
